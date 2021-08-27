dd9291991b7f3dff7160fb5b7e7754e0e0fd670b
Kawry Omar <komar2@huskers.unl.edu>
2021-08-27 19:47:46 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Kawry Omar
+ *         komar2@huskers.unl.edu
+ * Date: 2020/03/26
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Kawry Omar!\n");
+  printf("computer scince");
+  
+  return 0;
+}
